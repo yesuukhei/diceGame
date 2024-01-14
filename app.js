113beb200e80555ccf0777fcdeb156e542b9e784
Yesuukhei <yesuu1028@gmail.com>
2024-01-14 19:01:31 +0800

    JavaScriptiing anhni togloomoo amjilttai hiij duusgala.. :))

--- app.js
+++ app.js
@@ -1,26 +1,20 @@
-// Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 0;
+//togloomin buh gazart ashiglagdah global huvisagchid 
+var activePlayer;
+var scores;
+var roundScore;
 
-// Toglogchdin tsugluulsan onoog hadgalah huvisagch
-var scores = [0, 0];
+// Togloom duussan  esehiig hadgalah toluviin huvisagch
+var isGameOver;
 
-//Toglogchiin eeljindee tsugluulj baigaa onoog hadgalah huvisagch
-var roundScore = 0;
-
-
-// shooni ali talaaraa buusnig hadgalah huvisagch heregtei 1-6 onoo
-
-
-document.querySelector("#score-0").textContent = 0;
-document.querySelector("#score-1").textContent = 0;
-document.querySelector("#current-0").textContent = 0;
-document.querySelector("#current-1").textContent = 0;
 
 var diceDom = document.querySelector(".dice");
-diceDom.style.display = "none";
+
+initGame();
 
 document.querySelector(".btn-roll").addEventListener("click", function(){
-    var diceNumber = Math.floor(Math.random()*6) + 1;
+    if(isGameOver === false)
+    {
+        var diceNumber = Math.floor(Math.random()*6) + 1;
     diceDom.style.display = "block";
     diceDom.src = "dice-" + diceNumber + ".png";
 
@@ -32,45 +26,89 @@ document.querySelector(".btn-roll").addEventListener("click", function(){
         document.getElementById("current-" + activePlayer).textContent = roundScore; 
     }
     else {
-        //1 bolson uchir 0 bolgono
-        roundScore = 0;
-        document.getElementById("current-" + activePlayer).textContent = 0;
+        switchToNextPlayer();
 
-        //shoog alga bolgono
-        diceDom.style.display = "none";
-
-        // herev idevhtei toglogch 0 bwl idevhtei toglogchig 1 bolgo. Ugui bol idevhtei togloch 1 bwl tuuniig 0 bolgo
-
-        (activePlayer === 0) ? activePlayer = 1 : activePlayer = 0;
-
-        //ulaan tsegiig shiljuuleh
-        document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
-        
+}
     }
-
-})
+});
 
 // HOLD tovchni eventListener
 
 document.querySelector(".btn-hold").addEventListener("click",function(){
-    // ug toglogchin tsugluulsan eeljnii onoog global onoon deer ni nemeh
+    if(isGameOver === false)
+    {
+        // ug toglogchin tsugluulsan eeljnii onoog global onoon deer ni nemeh
     scores[activePlayer] += roundScore;
 
+    //delgets deer onoog ni uurchilno
     document.getElementById("score-" + activePlayer).textContent = scores[activePlayer];
 
-    //eeljin onoog 0 bolgoh
+
+    //ug toglogch hojson esehiig shalgah
+    if(scores[activePlayer] >= 10)
+    {
+        isGameOver = true;
+
+        document.getElementById("name-" + activePlayer).textContent = "WINNER!!!";
+        document.querySelector(".player-" + activePlayer +"-panel").classList.add("winner");
+        document.querySelector(".player-" + activePlayer +"-panel").classList.remove("active");
+    } else {
+        //toglogchiin eeljig solih
+         switchToNextPlayer();
+    }
+    }
+  
+})
+
+function switchToNextPlayer()
+{
+    //1 bolson uchir 0 bolgono
     roundScore = 0;
     document.getElementById("current-" + activePlayer).textContent = 0;
 
-    //toglogchiin eeljig solih
-    diceDom.style.display = "none";
+    // herev idevhtei toglogch 0 bwl idevhtei toglogchig 1 bolgo. Ugui bol idevhtei togloch 1 bwl tuuniig 0 bolgo
 
     (activePlayer === 0) ? activePlayer = 1 : activePlayer = 0;
 
+    //ulaan tsegiig shiljuuleh
     document.querySelector(".player-0-panel").classList.toggle("active");
-        document.querySelector(".player-1-panel").classList.toggle("active");
+    document.querySelector(".player-1-panel").classList.toggle("active");
+    
+     //shoog alga bolgono
+     diceDom.style.display = "none";
+}
+
+// Shine togloom ehluuleh tovchni evenListener
+document.querySelector(".btn-new").addEventListener("click", function(){
+    initGame();
+    diceDom.style.display = "none";
+});
+
+function initGame()
+{
+    isGameOver = false;
+
+    // Тоглогчийн ээлжийг хадгалах хувьсагч
+     activePlayer = 0;
+
+// Toglogchdin tsugluulsan onoog hadgalah huvisagch
+ scores = [0, 0];
+
+//Toglogchiin eeljindee tsugluulj baigaa onoog hadgalah huvisagch
+ roundScore = 0;
 
 
+// shooni ali talaaraa buusnig hadgalah huvisagch heregtei 1-6 onoo
 
-})
+document.querySelector("#score-0").textContent = 0;
+document.querySelector("#score-1").textContent = 0;
+document.querySelector("#current-0").textContent = 0;
+document.querySelector("#current-1").textContent = 0;
+
+document.getElementById("name-0").textContent = "Player 1";
+document.getElementById("name-1").textContent = "Player 2";
+
+document.querySelector(".player-0-panel").classList.remove("winner");
+document.querySelector(".player-1-panel").classList.remove("winner");
+document.querySelector(".player-0-panel").classList.add("active");
+}
